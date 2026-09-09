f030aec8de0b29359a574bfef21aedbf0c7c3cf7
gofraprod-debug <gofra.prod@gmail.com>
2026-09-09 22:18:24 +0300

    rebrand: AMN VPN (ru.amn.vpn) + chameleon icons

--- client/cmake/branding/android.cmake
+++ client/cmake/branding/android.cmake
@@ -1,3 +1,3 @@
 if(NOT CLIENT_ANDROID_PACKAGE)
-    set(CLIENT_ANDROID_PACKAGE "org.amnezia.vpn" CACHE STRING "Android package name for Play Store version lookup")
+    set(CLIENT_ANDROID_PACKAGE "ru.amn.vpn" CACHE STRING "Android package name for Play Store version lookup")
 endif()
